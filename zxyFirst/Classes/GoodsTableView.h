b5cc0da35669e75e5b6a9d13660d2e420d1e2215
朱鑫雨 <zhuxinyu@zhuxinyudeMacBook-Pro.local>
2022-09-27 17:02:13 +0800

    ddd

---
+++ zxyFirst/Classes/GoodsTableView.h
@@ -0,0 +1,13 @@
+//
+//  GoodsTableView.h
+//  XTWL_IOS_USER
+//
+//  Created by 开发003 on 2017/9/27.
+//  Copyright © 2017年 XTWL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoodsTableView : UITableView
+
+@end
---
+++ zxyFirst/Classes/GoodsTableView.m
@@ -0,0 +1,26 @@
+//
+//  GoodsTableView.m
+//  XTWL_IOS_USER
+//
+//  Created by 开发003 on 2017/9/27.
+//  Copyright © 2017年 XTWL. All rights reserved.
+//
+
+#import "GoodsTableView.h"
+
+@implementation GoodsTableView
+
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+
+    return YES;    
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
